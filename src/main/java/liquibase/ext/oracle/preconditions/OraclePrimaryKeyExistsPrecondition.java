4da1b324b8c2d99160ac95dc8fd7d273687a3006
Stuart Stephen <stuart.stephen@tracegroup.com>
2014-05-13 15:21:46 +0100

    Added check constraints, fk, pk and unique constraint support.

---
+++ src/main/java/liquibase/ext/oracle/preconditions/OraclePrimaryKeyExistsPrecondition.java
@@ -0,0 +1,88 @@
+package liquibase.ext.oracle.preconditions;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.exception.ValidationErrors;
+import liquibase.exception.Warnings;
+import liquibase.precondition.Precondition;
+
+public class OraclePrimaryKeyExistsPrecondition extends OraclePrecondition {
+
+	private String primaryKeyName;
+	private String tableName;
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName( String tableName ) {
+		this.tableName = tableName;
+	}
+
+	public String getName() {
+		return "oraclePrimaryKeyExists";
+	}
+
+	public String getPrimaryKeyName() {
+		return primaryKeyName;
+	}
+
+	public void setPrimaryKeyName( String constraintName ) {
+		this.primaryKeyName = constraintName;
+	}
+
+	public Warnings warn( Database database ) {
+		return new Warnings();
+	}
+
+	public ValidationErrors validate( Database database ) {
+		return new ValidationErrors();
+	}
+
+	public void check( Database database, DatabaseChangeLog changeLog, ChangeSet changeSet ) throws PreconditionFailedException, PreconditionErrorException {
+		JdbcConnection connection = (JdbcConnection) database.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			/*
+			THE CONSTRAINT_TYPE will tell you what type of constraint it is
+			
+			R - Referential key ( foreign key)
+			U - Unique key
+			P - Primary key
+			C - Check constraint
+		 */
+			final String sql = "select count(*) from all_constraints where upper(constraint_name) = upper(?) and table_name = upper(?) and upper(owner) = upper(?) and constraint_type = 'P'";
+			ps = connection.prepareStatement( sql );
+			ps.setString( 1, getPrimaryKeyName() );
+			ps.setString( 2, getTableName() );
+			ps.setString( 3, database.getLiquibaseSchemaName() );
+			rs = ps.executeQuery();
+			if ( !rs.next() || rs.getInt( 1 ) <= 0 ) {
+				throw new PreconditionFailedException( String.format( "The primary key '%s' was not found on the table '%s.%s'.", getPrimaryKeyName(), database.getLiquibaseSchemaName(), getTableName() ), changeLog, this );
+			}
+		} catch ( SQLException e ) {
+			throw new PreconditionErrorException( e, changeLog, this );
+		} catch ( DatabaseException e ) {
+			throw new PreconditionErrorException( e, changeLog, this );
+		} finally {
+			closeSilently( rs );
+			closeSilently( ps );
+		}
+	}
+
+}
